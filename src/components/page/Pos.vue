99d3c5a81131bd755818f187022b75a134236413
liujiangbo <liujiangbo@megvii.com>
2019-06-28 15:00:38 +0800

    add source

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span='7'>
+         订单栏
+      </el-col>
+      <el-col :span='7'>
+         产品栏
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
